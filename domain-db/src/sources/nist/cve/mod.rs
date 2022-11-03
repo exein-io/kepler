2f8b5b8e97d1cc2796195ff103a85896edd14a6c
banditopazzo <banditopazzo@gmail.com>
2022-11-03 14:58:18 +0100

    chore: clean logs

--- domain-db/src/sources/nist/cve/mod.rs
+++ domain-db/src/sources/nist/cve/mod.rs
@@ -1,11 +1,9 @@
 use anyhow::{Context, Result};
+use flate2::read::GzDecoder;
 use std::fs::{self, File};
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
 use std::time::Instant;
-
-use flate2::read::GzDecoder;
-use log::info;
 
 use crate::sources::download_to_file;
 
@@ -23,13 +21,13 @@ pub fn setup(year: &str, data_path: &Path, fresh: bool) -> Result<(PathBuf, Vec<
 
     if fresh {
         if gzip_file_name.exists() {
-            info!("removing {}", gzip_file_name.display());
+            log::info!("removing {}", gzip_file_name.display());
             fs::remove_file(&gzip_file_name)
                 .with_context(|| format!("could not remove {}", gzip_file_name.display()))?;
         }
 
         if file_name.exists() {
-            info!("removing {}", file_name.display());
+            log::info!("removing {}", file_name.display());
             fs::remove_file(&file_name)
                 .with_context(|| format!("could not remove {}", file_name.display()))?;
         }
@@ -43,19 +41,19 @@ pub fn setup(year: &str, data_path: &Path, fresh: bool) -> Result<(PathBuf, Vec<
             );
             download_to_file(&url, &gzip_file_name)?;
         } else {
-            info!("found {}", gzip_file_name.display());
+            log::info!("found {}", gzip_file_name.display());
         }
         gunzip(&gzip_file_name, &file_name)?;
     } else {
-        info!("found {}", file_name.display());
+        log::info!("found {}", file_name.display());
     }
 
-    info!("reading {} ...", file_name.display());
+    log::info!("reading {} ...", file_name.display());
 
     let start = Instant::now();
     let cve_list = read_cves_from_path(&file_name)?;
 
-    info!("loaded {} CVEs in {:?}", cve_list.len(), start.elapsed());
+    log::info!("loaded {} CVEs in {:?}", cve_list.len(), start.elapsed());
 
     Ok((file_name, cve_list))
 }
@@ -77,12 +75,13 @@ fn gunzip(from: &Path, to: &Path) -> Result<()> {
 }
 
 pub fn read_cves_from_path<P: AsRef<Path>>(path: P) -> Result<Vec<item::CVE>> {
-    let file = File::open(&path).context("failed to open file")?;
+    let file = File::open(&path)
+        .with_context(|| format!("failed to open file {}", path.as_ref().display()))?;
 
     let reader = BufReader::new(file);
 
-    let mut cves: Vec<item::CVE> =
-        serde_json::from_reader(reader).context("failed to parse cves")?;
+    let mut cves: Vec<item::CVE> = serde_json::from_reader(reader)
+        .with_context(|| format!("failed to parse cves from {}", path.as_ref().display()))?;
 
     // remove CVE without configurations as they're still being processed
     cves.retain(|item| item.is_complete());
